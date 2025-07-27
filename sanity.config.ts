a6fd4f3b909f6a10e582bca6628ada6388f18075
yutaro <131897266+yutaro21jp@users.noreply.github.com>
2025-07-27 13:50:59 +0700

    Studio側で軽微な変更を行いました。

--- sanity.config.ts
+++ sanity.config.ts
@@ -15,6 +15,8 @@ import {structure} from './sanity/structure'
 
 export default defineConfig({
   basePath: '/studio',
+  name: 'btc-insight',
+  title: 'BTCインサイト',
   projectId,
   dataset,
   // Add and edit the content schema in the './sanity/schemaTypes' folder
