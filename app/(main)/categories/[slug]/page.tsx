9ce9ffac83d1e52336c8d59d2d566ef8b74c45aa
yutaro <131897266+yutaro21jp@users.noreply.github.com>
2025-07-27 17:05:39 +0700

    ビルドが成功しました！

--- app/(main)/categories/[slug]/page.tsx
+++ app/(main)/categories/[slug]/page.tsx
@@ -14,8 +14,10 @@ type Post = {
   slug: { current: string }
   publishedAt: string
   mainImage?: {
+    _type: 'image',
     asset: {
-      _ref: string
+      _ref: string,
+      _type: 'reference'
     }
   }
 }
--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -11,8 +11,10 @@ type Post = {
   slug: { current: string }
   publishedAt: string
   mainImage?: {
+    _type: 'image',
     asset: {
-      _ref: string
+      _ref: string,
+      _type: 'reference'
     }
   }
 }
